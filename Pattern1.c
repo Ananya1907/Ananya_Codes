796cf8b9ae4b91b7cb3b9396f958bdeedf0e5591
Ananya1907 <143334154+Ananya1907@users.noreply.github.com>
2024-11-14 20:02:34 +0530

    Update and rename Examples.c to Pattern1.c
    Add comments

--- Pattern1.c
+++ Pattern1.c
@@ -4,6 +4,8 @@
 #include<ctype.h>
 #include<math.h>
 
+// Input: a3b5H2
+// Output: aaabbbbbHH
 void main(){
     char letter, *str;
     int n, num, j, k, i=0;
@@ -11,14 +13,14 @@ void main(){
     scanf("%s", str);
     n=strlen(str);
     while(i<n){
-        if(isalpha(str[i])){
-            letter=str[i];
+        if(isalpha(str[i])){        // checks if the input is an alphabet,
+            letter=str[i];          // and stores it in letter to print later
             i+=1;
         }
         else {
             num=0;
-            while(isdigit(str[i])){
-                num= num*10+(str[i]-'0');
+            while(isdigit(str[i])){           // checks if input is a digit,
+                num= num*10+(str[i]-'0');     // and stores the number of times letter is to be printed in num
                 i+=1;
             }
             for(j=0; j<num; j++) printf("%c", letter);
